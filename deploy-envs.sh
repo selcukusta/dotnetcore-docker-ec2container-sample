92cee5309e47d96c32d023dc01ac9135bb5d658b
Selçuk Usta <susta@hurriyet.com.tr>
2017-05-17 15:44:05 +0300

    Change AWS_VIRTUAL_HOST

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=ec2-34-210-194-241.us-west-2.compute.amazonaws.com
+export AWS_VIRTUAL_HOST=34.210.194.241
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
