18e47de6195372d1548ee0500273faa7d95f51ac
Selçuk Usta <susta@hurriyet.com.tr>
2017-05-17 18:35:28 +0300

    Update deploy-envs.sh

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=netcore-sampleapp
 export IMAGE_VERSION=latest
+
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=34.210.194.241
